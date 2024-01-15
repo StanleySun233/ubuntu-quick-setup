669673488ecf680fb76501c774f6a32eeb65ebe3
Stanley Sun <75790014+StanleySun233@users.noreply.github.com>
2024-01-15 10:17:36 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+sudo apt update
+
+sudo apt upgrade -y
+
+curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+
+sh setup_docker.sh
