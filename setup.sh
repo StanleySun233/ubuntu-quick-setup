ef43428ebed2c796b1f13ff7dd0c8dd72581499c
Stanley Sun <75790014+StanleySun233@users.noreply.github.com>
2024-06-07 16:10:10 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ sudo apt upgrade -y
 curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
 
-# install 1Panel
-curl -sSL https://resource.fit2cloud.com/1panel/package/quick_start.sh -o quick_start.sh && sudo bash quick_start.sh
-
 # install docker image
 sh setup_docker.sh
+
+# install 1Panel
+curl -sSL https://resource.fit2cloud.com/1panel/package/quick_start.sh -o quick_start.sh && sudo bash quick_start.sh
